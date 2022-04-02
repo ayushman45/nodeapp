2883b9787d8c6732de15d77e2bbb56c7b473e99d
ayushman45 <ayushman.manishankar@gmail.com>
2022-04-02 12:29:12 +0530

    login page added

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/",function(req,res){
     res.send(" Working ")
 })
 
+app.get("/login",function(req,res){
+    res.send("Login Page")
+})
+
 app.listen(process.env.PORT||3000,function(req,res){
     console.log("Server Started on port 3000 successfully");
 })
